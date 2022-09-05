1a0c3424a26c420fd83b4a7a3a709f49e4bc93c6
Rail <wanglifei@yiqicai.com>
2022-09-05 17:00:14 +0800

    feat: 类型层级

--- index.ts
+++ index.ts
@@ -405,10 +405,10 @@ type Conditional<Type, Condition, TruthyResult, FalsyResult> =
   Type extends Condition ? TruthyResult : FalsyResult;
 
 //  "passed!"
-type Result1 = Conditional<'linbudu', string, 'passed!', 'rejected!'>;
+// type Result1 = Conditional<'linbudu', string, 'passed!', 'rejected!'>;
 
 // "rejected!"
-type Result2 = Conditional<'linbudu', boolean, 'passed!', 'rejected!'>;
+// type Result2 = Conditional<'linbudu', boolean, 'passed!', 'rejected!'>;
 
 
 type ProcessInput<
@@ -662,3 +662,177 @@ export type Opaque<Type, Token = unknown> = Type & Tagged<Token>;
 
 
 
+// 类型层级开始
+
+type Result = 'linbudu' extends string ? 1 : 2;
+
+
+declare let source: string;
+
+declare let anyType: any;
+declare let neverType: never;
+
+
+anyType = source;
+
+// 不能将类型“string”分配给类型“never”。
+neverType = source;
+
+
+
+type Result1 = "linbudu" extends string ? 1 : 2; // 1
+type Result2 = 1 extends number ? 1 : 2; // 1
+type Result3 = true extends boolean ? 1 : 2; // 1
+type Result4 = { name: string } extends object ? 1 : 2; // 1
+type Result5 = { name: 'linbudu' } extends object ? 1 : 2; // 1
+type Result6 = [] extends object ? 1 : 2; // 1
+
+
+//联合类型
+type Result7 = 1 extends 1 | 2 | 3 ? 1 : 2; // 1
+type Result8 = 'lin' extends 'lin' | 'bu' | 'du' ? 1 : 2; // 1
+type Result9 = true extends true | false ? 1 : 2; // 1
+
+//结论：字面量类型 < 包含此字面量类型的联合类型，原始类型 < 包含此原始类型的联合类型。
+
+
+type Result11 = 'lin' | 'bu' | 'budu' extends string ? 1 : 2; // 1
+type result12 = [] | {} | (()=>void) extends object?1:2
+
+//结论：同一基础类型的字面量联合类型 < 此基础类型。
+
+// 最终结论：字面量类型 < 包含此字面量类型的联合类型（同一基础类型） < 对应的原始类型，即：
+
+type Result13 = 'linbudu' extends 'linbudu' | '599'
+  ? 'linbudu' | '599' extends string
+    ? 2
+    : 1
+  : 0;
+//装箱类型
+type Result14 = string extends String ? 1 : 2; // 1
+type Result15 = String extends {} ? 1 : 2; // 1
+// type Result16 = {} extends object ? 1 : 2; // 1
+// type Result18 = object extends Object ? 1 : 2; // 1
+
+
+type Tmp = string extends object ? 1 : 2; // 2
+
+
+type Result16 = {} extends object ? 1 : 2;
+type Result18 = object extends {} ? 1 : 2;
+
+type Result17 = object extends Object ? 1 : 2;
+type Result20 = Object extends object ? 1 : 2;
+
+type Result19 = Object extends {} ? 1 : 2;
+type Result21 = {} extends Object ? 1 : 2;
+
+type test1 = number extends Object?1:2
+type test2 = number extends object?1:2
+
+
+
+// top type
+// any unknown
+
+type Result22 = Object extends any ? 1 : 2; // 1
+type Result23 = Object extends unknown ? 1 : 2; // 1
+
+type Result24 = any extends Object ? 1 : 2; // 1 | 2
+type Result25 = unknown extends Object ? 1 : 2; // 2
+
+
+type Result26 = any extends 'linbudu' ? 1 : 2; // 1 | 2
+type Result27 = any extends string ? 1 : 2; // 1 | 2
+type Result28 = any extends {} ? 1 : 2; // 1 | 2
+type Result29 = any extends never ? 1 : 2; // 1 | 2
+
+
+type Result31 = any extends unknown ? 1 : 2;  // 1
+type Result32 = unknown extends any ? 1 : 2;  // 1
+
+//结论 Object < any / unknown
+
+//类型系统 向下探索
+
+type Result33 = never extends 'linbudu' ? 1 : 2; // 1
+
+
+type Result34 = undefined extends 'linbudu' ? 1 : 2; // 2
+type Result35 = null extends 'linbudu' ? 1 : 2; // 2
+type Result36 = void extends 'linbudu' ? 1 : 2; // 2
+
+//never < 字面量类型
+
+
+type TypeChain = never extends 'linbudu'
+  ? 'linbudu' extends 'linbudu' | '599'
+  ? 'linbudu' | '599' extends string
+  ? string extends String
+  ? String extends Object
+  ? Object extends any
+  ? any extends unknown
+  ? unknown extends any
+  ? 8
+  : 7
+  : 6
+  : 5
+  : 4
+  : 3
+  : 2
+  : 1
+  : 0
+
+  type VerboseTypeChain = never extends 'linbudu'
+  ? 'linbudu' extends 'linbudu' | 'budulin'
+  ? 'linbudu' | 'budulin' extends string
+  ? string extends {}
+  ? string extends String
+  ? String extends {}
+  ? {} extends object
+  ? object extends {}
+  ? {} extends Object
+  ? Object extends {}
+  ? object extends Object
+  ? Object extends object
+  ? Object extends any
+  ? Object extends unknown
+  ? any extends unknown
+  ? unknown extends any
+  ? 8
+  : 7
+  : 6
+  : 5
+  : 4
+  : 3
+  : 2
+  : 1
+  : 0
+  : -1
+  : -2
+  : -3
+  : -4
+  : -5
+  : -6
+  : -7
+  : -8
+
+
+  //其他比较场景
+
+type Result360 = 1 | 2 | 3 extends 1 | 2 | 3 | 4 ? 1 : 2; // 1
+type Result37 = 2 | 4 extends 1 | 2 | 3 | 4 ? 1 : 2; // 1
+type Result38 = 1 | 2 | 5 extends 1 | 2 | 3 | 4 ? 1 : 2; // 2
+type Result39 = 1 | 5 extends 1 | 2 | 3 | 4 ? 1 : 2; // 2
+
+
+//数组和元组
+type Result40 = [number, number] extends number[] ? 1 : 2; // 1
+type Result41 = [number, string] extends number[] ? 1 : 2; // 2
+type Result42 = [number, string] extends (number | string)[] ? 1 : 2; // 1
+type Result43 = [] extends number[] ? 1 : 2; // 1
+type Result44 = [] extends unknown[] ? 1 : 2; // 1
+type Result45 = number[] extends (number | string)[] ? 1 : 2; // 1
+type Result46 = any[] extends number[] ? 1 : 2; // 1
+type Result47 = unknown[] extends number[] ? 1 : 2; // 2
+type Result48 = never[] extends number[] ? 1 : 2; // 1

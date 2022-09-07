801c999b0e0abe09e9439bb41f0f4b83c004815f
Rail <wanglifei@yiqicai.com>
2022-09-07 19:10:26 +0800

    feat: 类型体操

--- index.ts
+++ index.ts
@@ -971,13 +971,9 @@ type Obj = ReverseKeyVal<{2:'str'}>
 //     : V
 //   : T;
 
-type PromiseValue<T> = T extends Promise<infer V> ? PromiseValue<V> : T;
+// type PromiseValue<T> = T extends Promise<infer V> ? PromiseValue<V> : T;
 
-type PromiseValueResult1 = PromiseValue<Promise<number>>; // number
 
-type PromiseValueResult2 = PromiseValue<number>; // number，但并没有发生提取
-
-type PromiseValueResult3 = PromiseValue<Promise<Promise<boolean>>>; // Promise<boolean>，只提取了一层
 
 
 
@@ -1288,3 +1284,16 @@ type CheckArgType = AsFunArgType<Animal> extends AsFunArgType<Dog> ? 1 : 2;
 
 
 //进行一个总结：函数类型的参数类型使用子类型逆变的方式确定是否成立，而返回值类型使用子类型协变的方式确定。
+
+
+type PromiseValue<T> = T extends Promise<infer V>?PromiseValue<V>:T
+
+type PromiseValueResult1 = PromiseValue<Promise<number>>; // number
+
+type PromiseValueResult2 = PromiseValue<number>; // number，但并没有发生提取
+
+type PromiseValueResult3 = PromiseValue<Promise<Promise<boolean>>>; // Promise<boolean>，只提取了一层
+
+
+
+
---
+++ tc.ts
@@ -0,0 +1,70 @@
+import {expectType} from 'tsd';
+
+type DeepPartial<T extends object> = {
+     [K in keyof T]?: T[K] extends object ? DeepPartial<T[K]> : T[K];
+ };
+
+ type DeepPartialStruct = DeepPartial<{
+   foo: string;
+   nested: {
+     nestedFoo: string;
+     nestedBar: {
+       nestedBarFoo: string;
+     };
+   };
+ }>;
+
+
+ 
+ expectType<DeepPartialStruct>({
+   foo: 'bar',
+   nested: {},
+ });
+ 
+ expectType<DeepPartialStruct>({
+   nested: {
+     nestedBar: {},
+   },
+ });
+ 
+ expectType<DeepPartialStruct>({
+   nested: {
+     nestedBar: {
+       nestedBarFoo: 'test',
+     },
+   },
+ });
+
+
+
+ type DeepRequired<T extends object> = {
+  [K in keyof T]-?: T[K] extends object ? DeepRequired<T[K]> : T[K];
+ }
+
+ type DeepRequiredStruct = DeepRequired<{
+  foo: string;
+  nested: {
+    nestedFoo: string;
+    nestedBar: {
+      nestedBarFoo: string;
+    };
+  };
+}>;
+
+
+ 
+expectType<DeepRequiredStruct>({
+  foo:'foo',
+  nested: {
+    nestedFoo: 'test',
+    nestedBar: {
+      nestedBarFoo: 'test',
+    },
+  },
+});
+
+
+type DeepReadonly<T extends object> = {
+  readonly [K in keyof T]:T[K] extends object?DeepReadonly<T[K]>:T[K]
+}
+ 
